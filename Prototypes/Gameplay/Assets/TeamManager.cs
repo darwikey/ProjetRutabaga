a73ebbea46bdeaa9622046ee280ffd25e0ab35af
darwikey <xavier.maupeu@wanadoo.fr>
2015-11-21 22:56:08 +0100

    ajout proto gameplay

---
+++ Prototypes/Gameplay/Assets/TeamManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class TeamManager : MonoBehaviour {
+
+    List<GameObject> _team1 = new List<GameObject>();
+    List<GameObject> _team2 = new List<GameObject>();
+
+    public uint _numPlayers = 5;
+
+    public GameObject _playerEntity;
+
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (_team1.Count() < _numPlayers)
+        {
+            SpawnPlayer(1);
+        }
+
+        if (_team2.Count() < _numPlayers)
+        {
+            SpawnPlayer(2);
+        }
+    }
+
+
+    void SpawnPlayer(int team)
+    {
+        Vector3 position = new Vector3(Random.Range(0.0f, 200.0f), 1.0f, Random.Range(0.0f, 200.0f));
+        GameObject player = Instantiate(_playerEntity, position, Quaternion.identity) as GameObject;
+
+        GameObject minimapIcon = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        minimapIcon.transform.SetParent(player.transform);
+        minimapIcon.transform.localPosition = new Vector3(0.0f, 1.5f, 0.0f);
+        minimapIcon.transform.rotation = Quaternion.Euler(90.0f, 0.0f, 0.0f);
+        minimapIcon.transform.localScale = 9.0f * Vector3.one;
+        minimapIcon.layer = 10;
+
+        if (team == 1)
+        {
+            if (_team1.Count() == 0)
+            {
+                player.GetComponent<ThirdPersonController>().enabled = true;
+            }
+            _team1.Add(player);
+        }
+        else
+        {
+            _team2.Add(player);
+        }
+    }
+}
---
+++ Prototypes/Gameplay/Assets/Tower.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tower : MonoBehaviour {
+
+	public GameObject _player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((transform.position - _player.transform.position).sqrMagnitude < 150.0f) {
+
+		}
+	}
+}
