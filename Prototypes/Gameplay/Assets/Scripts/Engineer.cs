c28fd0bf8edd440d93f39f9e05b44ef633952d01
darwikey <xavier.maupeu@wanadoo.fr>
2015-11-22 10:01:43 +0100

    ajout des classes de base pour les joueurs

---
+++ Prototypes/Gameplay/Assets/Scripts/Engineer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Engineer : Player
+{
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Prototypes/Gameplay/Assets/Scripts/Protector.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Protector : Player
+{
+
+   
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
